7b4e46b2b489652d14e4172d503dcfe9b530cc1a
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-05-26 12:46:25 +0500

    Use environment variable for PORT configuration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
